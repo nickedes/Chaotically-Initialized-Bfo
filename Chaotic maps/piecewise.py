1441d90ba246cb4137d7f132867f837933e40e94
nikhil <nkmittal4994@gmail.com>
2016-01-28 23:19:15 +0530

    Pep8-ify! and added value of P P is a parameter in piecewise

--- Chaotic maps/piecewise.py
+++ Chaotic maps/piecewise.py
@@ -1,11 +1,11 @@
 def piecewise(x):
     # define p
-    p = 0
+    p = 0.4
     if 0 <= x < p:
-    	return x/p
+        return x/p
     elif p <= x < 0.5:
-    	return (x-p)/(0.5-p)
+        return (x-p)/(0.5-p)
     elif 0.5 <= x < 1-p:
-    	return (1-p-x)/(0.5-p)
+        return (1-p-x)/(0.5-p)
     elif 1-p <= x < 1:
-    	return (1-x)/p
+        return (1-x)/p
