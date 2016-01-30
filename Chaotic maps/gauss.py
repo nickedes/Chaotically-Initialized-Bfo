a9fc49f319e416cd7733bd289cc3ae5cfd03432e
nikhil <nkmittal4994@gmail.com>
2016-01-30 15:48:01 +0530

    correct definition of Gauss map

--- Chaotic maps/gauss.py
+++ Chaotic maps/gauss.py
@@ -4,8 +4,7 @@ from math import floor as f
 def gauss(x):
     if x == 0:
         return 0
-    elif 0 < x < 1:
-        return 1/x - f(1/x)
     else:
-        print(x)
-        raise ValueError
+        return 1/x - f(1/x)
+
+print(gauss(1.2))
