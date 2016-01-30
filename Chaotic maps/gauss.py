881dedd3b9c67979da6b0b63becf9717b21e0137
nikhil <nkmittal4994@gmail.com>
2016-01-30 11:44:25 +0530

    minor changes--pep8-ify!

--- Chaotic maps/gauss.py
+++ Chaotic maps/gauss.py
@@ -1,6 +1,8 @@
 from math import floor as f
+
+
 def gauss(x):
-	if x == 0:
-		return 0
-	elif 0 < x < 1:
-		return 1/x - f(1/x)
+    if x == 0:
+        return 0
+    elif 0 < x < 1:
+        return 1/x - f(1/x)
