c2c4ea981d30a2975c20d85bf4e13b75d3b980b5
nikhil <nkmittal4994@gmail.com>
2016-01-31 13:15:31 +0530

    Normalized values before applying sphere.

--- Benchmark functions/sphere.py
+++ Benchmark functions/sphere.py
@@ -1,16 +1,18 @@
 # SPHERE FUNCTION
-
 # Input Domain: [-5.12, 5.12]
 
+
 def normalization(x, x_min=-5.12, x_max=5.12):
-	"""
-	"""
-	for i in range(len(x)):
-		x[i] = (x[i]−x_min)/(x_max−x_min)
-	return x
+    """
+    """
+    for i in range(len(x)):
+        x[i] = (x[i]-x_min)/(x_max-x_min)
+    return x
+
 
 def sphere(x):
-	sum_square = 0
-	for elem in x:
-		sum_square += elem**2
-	return sum_square 
+    x = normalization(x)
+    sum_square = 0
+    for elem in x:
+        sum_square += elem**2
+    return sum_square
