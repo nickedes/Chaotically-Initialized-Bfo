ed0939b18e5bc9ae4f3ea6caab1b0220dd60a0e5
nikhil <nkmittal4994@gmail.com>
2016-02-01 02:08:37 +0530

    Full definition of rastrigin

--- Benchmark functions/rastrigin.py
+++ Benchmark functions/rastrigin.py
@@ -1,8 +1,12 @@
 # RASTRIGIN FUNCTION
 # Input Domain: [-5.12, 5.12]
-
+from math import cos, pi
 from sphere import normalization
 
 
 def rastrigin(x):
-    return x
+    x = normalization(x)
+    result = 0
+    for elem in x:
+        result += elem**2 - 10*cos(2*pi*elem) + 10
+    return result
