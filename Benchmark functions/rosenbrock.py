0263954f44d4c794a63ba49fcbdb9e76cf12e7fc
nikhil <nkmittal4994@gmail.com>
2016-02-01 23:41:32 +0530

    Pep8-ify! :tada:

--- Benchmark functions/rosenbrock.py
+++ Benchmark functions/rosenbrock.py
@@ -2,9 +2,11 @@
 # Input Domain: [-2.048, 2.048]
 from sphere import normalization
 x_min, x_max = -2.048, 2.048
+
+
 def rosenbrock(x):
-	x = normalization(x, x_min, x_max)
-	result = 0
-	for i in range(len(x)-1):
-		result += 100*(x[i+1]-x[i]**2)**2 + (x[i] - 1)**2
-	return result
+    x = normalization(x, x_min, x_max)
+    result = 0
+    for i in range(len(x)-1):
+        result += 100*(x[i+1]-x[i]**2)**2 + (x[i] - 1)**2
+    return result
