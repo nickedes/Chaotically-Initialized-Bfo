54529eaf2f16ced283b2ccf238b5ee46bc4d491b
nikhil <nkmittal4994@gmail.com>
2016-02-08 19:11:06 +0530

    MOre to initialization..initialise space

--- Bfo/init.py
+++ Bfo/init.py
@@ -3,6 +3,15 @@ from math import pi
 
 INF = float("inf")
 dimension = 10
+
+
+class Cell:
+    vect = [0]*dimension      # position in search space
+    cost = 0.0                # objective function value
+    fitness = 0.0             # cost value and attractant and repellent effects
+    health = 0.0              # the health of bacterium
+    step_size = 0.0           # step in the search area
+
 S = 50      # population size
 Sr = S/2     # number to split
 ss = 0.6     # step size
@@ -16,12 +25,20 @@ w_attr = 0.2     # width of the attractant signal
 h_rep = d_attr  # height of the repellant effect
 w_rep = 10.0    # width of the repellant
 
-
-class Cell:
-    vect = [0]*dimension      # position in search space
-    cost = 0.0                # objective function value
-    fitness = 0.0             # cost value and attractant and repellent effects
-    health = 0.0              # the health of bacterium
-    step_size = 0.0           # step in the search area
-
 population = [Cell() for i in range(S)]  # population of bacteria
+
+space = [[0]*2]*dimension  # the boundaries of the search space
+rand_vect = [0]*dimension  # direction of movement after a tumble
+delta = [0]*dimension      # used in the normalization of the rand_vect
+
+best = INF                 # the best solution found during the search
+fe_count = 0               # number of objective function evaluations
+
+
+def initialize_space(a, b):
+    """
+    set the bounds values for search space.
+    """
+    for i in range(dimension):
+        space[i][0] = a
+        space[i][1] = b
