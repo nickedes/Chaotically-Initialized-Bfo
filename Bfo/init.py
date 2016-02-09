8348f4c8c73ff97c59b850fc8efc5b89e60187b3
nikhil <nkmittal4994@gmail.com>
2016-02-09 22:17:53 +0530

    minor changes

--- Bfo/init.py
+++ Bfo/init.py
@@ -15,9 +15,9 @@ class Cell:
 S = 100      # population size
 Sr = S//2     # number to split
 ss = 0.6     # step size
-N_ed = 3       # number of elimination-dispersal events
-N_re = 6       # number of reproduction steps
-N_ch = 10      # number of chemotactic steps
+N_ed = 12      # number of elimination-dispersal events
+N_re = 10       # number of reproduction steps
+N_ch = 15      # number of chemotactic steps
 N_sl = 4       # swim length
 p_ed = 0.25    # eliminate probability
 d_attr = 0.1     # depth of the attractant
@@ -33,7 +33,8 @@ delta = [0]*dimension      # used in the normalization of the rand_vect
 
 
 def random_val(a, b):
-    num = a + random()*(b-a)
+    num = random()
+    num = a + num*(b-a)
     return num
 
 
