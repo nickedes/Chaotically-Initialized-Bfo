dd104b2a9ec8217ced46327fe34f1dc2ac5de118
nikhil <nkmittal4994@gmail.com>
2016-02-11 21:26:38 +0530

    Sphere added as fitness function

--- Bfo/Random/fitness.py
+++ Bfo/Random/fitness.py
@@ -1,1 +1,28 @@
 """Fitness Functions."""
+
+
+def normalization(x, x_min=-5.12, x_max=5.12):
+    """
+    For normalization of data within specified Input Domain.
+    """
+    for i in range(dimension):
+        x.vect[i] = x_min + x.vect[i]*(x_max-x_min)
+    return x
+
+
+def sphere(x, fe_count, best):
+    """
+    SPHERE FUNCTION
+    Input Domain: [-5.12, 5.12]
+    """
+    x = normalization(x)
+    rez = 0.0
+    fe_count = fe_count + 1
+    for i in range(dimension):
+        rez += pow(x.vect[i], 2.0)
+
+    x.cost = rez
+
+    if x.cost < best:
+        best = x.cost
+    return x, fe_count, best
