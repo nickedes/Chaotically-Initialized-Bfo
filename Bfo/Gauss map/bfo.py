b1bd3aeec9d41fb4d0179aa54503e2641f2533dc
nikhil <nkmittal4994@gmail.com>
2016-02-17 17:19:27 +0530

    INitial random settting

---
+++ Bfo/Gauss map/bfo.py
@@ -0,0 +1,18 @@
+from init import *
+from optimization import *
+
+num = 8  # number of fitness functions
+
+if __name__ == '__main__':
+    print("Bacterial Foraging Optimization Algorithm")
+    print("Dimension: ", dimension)
+    for i in range(num):
+        best = INF                 # the best solution found during the search
+        fe_count = 0               # number of objective function evaluations
+        # search space [-100, 100]^dimension
+        c_space, c_prob, space = initialize_space(space, -100.0, 100.0)
+        # random initialization within the search space
+        population, c_space, fe_count, best = initialize_population(
+            i+1, population, c_space, fe_count, best)
+        # minimization of objective function
+        optimization(i+1, population, c_space, fe_count, best, c_prob)
