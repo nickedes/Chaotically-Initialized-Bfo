ddf1c16f7f0aeaf2a57f6c910ac2990d703d8b19
nikhil <nkmittal4994@gmail.com>
2016-02-29 18:06:40 +0530

    More Maps added

--- Bfo/Sphere/maps.py
+++ Bfo/Sphere/maps.py
@@ -1,5 +1,42 @@
+from math import floor as f
+
+
 def logistic(x, a=0.4):
     x = a*x*(1-x)
+    return x
+
+
+def gauss(x):
+    if x == 0:
+        x = 0
+    else:
+        x = 1/x - f(1/x)
+    return x
+
+
+def piecewise(x, p=0.4):
+    if 0 <= x < p:
+        x = x/p
+    elif p <= x < 0.5:
+        x = (x-p)/(0.5-p)
+    elif 0.5 <= x < 1-p:
+        x = (1-p-x)/(0.5-p)
+    elif 1-p <= x < 1:
+        x = (1-x)/p
+    return x
+
+
+def singer(x, u=1.07):
+    if 0 < x < 1:
+        x = u*(7.86*x - 23.31*x**2 + 28.75*x**3 - 13.302875*x**4)
+        return x
+
+
+def tent(x, a=0.7):
+    if x < a:
+        x = x/a
+    elif x >= a:
+        x = (1-x)/(1-a)
     return x
 
 
@@ -9,3 +46,21 @@ def map_func(num, x):
     """
     if num == 1:
         return logistic(x)
+    elif num == 2:
+        return gauss(x)
+    elif num == 3:
+        return piecewise(x)
+    elif num == 4:
+        return singer(x)
+    elif num == 5:
+        return tent(x)
+    elif num == 6:
+        return gauss(x)
+    elif num == 7:
+        return gauss(x)
+    elif num == 8:
+        return gauss(x)
+    elif num == 9:
+        return gauss(x)
+    elif num == 10:
+        return gauss(x)
