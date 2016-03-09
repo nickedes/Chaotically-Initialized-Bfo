9d374ddadc3e39e02e73b5afac4260fd45574a59
nikhil <nkmittal4994@gmail.com>
2016-03-09 11:40:23 +0530

    data collection for variations of cpsace ctumble cprob

--- Bfo/Rosenbrock/init.py
+++ Bfo/Rosenbrock/init.py
@@ -35,8 +35,8 @@ delta = [0]*dimension      # used in the normalization of the rand_vect
 
 # chaotic initializations
 c_prob = 0.8   # chaotic init for probability
-c_space = 0.1   # chaotic init for space
-c_tumble = 0.17  # chaotic init for tumbling
+c_space = 0.3   # chaotic init for space
+c_tumble = 0.5  # chaotic init for tumbling
 
 
 def rosenbrock(x, fe_count, best):
