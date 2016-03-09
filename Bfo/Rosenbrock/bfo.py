4135a9ed006563c3a24d0a5b6df23d79c5c23dce
nikhil <nkmittal4994@gmail.com>
2016-03-09 11:31:34 +0530

    Combining inits with the algo and saving results.

---
+++ Bfo/Rosenbrock/bfo.py
@@ -0,0 +1,31 @@
+from init import *
+from optimization import *
+from json import dumps
+
+num = 10  # number of Chaotic maps
+
+if __name__ == '__main__':
+    print("Bacterial Foraging Optimization Algorithm")
+    print("Dimension: ", dimension)
+    # For tracking global minima. ;)
+    results = {'c_space': c_space, 'c_prob': c_prob, 'c_tumble': c_tumble}
+    for i in range(num):
+        best = INF                 # the best solution found during the search
+        fe_count = 0               # number of objective function evaluations
+        # search space [-100, 100]^dimension
+        c_tumble, c_space, c_prob, space = initialize_space(
+            space, -100.0, 100.0)
+        # random initialization within the search space
+        population, c_space, fe_count, best = initialize_population(
+            i+1, population, c_space, fe_count, best)
+        # minimization of objective function
+        best = optimization(
+            i+1, population, c_space, fe_count, best, c_prob, c_tumble)
+        if best == 0.0:
+            results[str(i+1)] = 1.0
+        else:
+            results[str(i+1)] = best
+
+    with open('data.py', 'a') as f:
+        f.write(dumps(results, sort_keys=True) + '\n')
+    print("Updated!")
