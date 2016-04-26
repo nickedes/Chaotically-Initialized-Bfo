b493984d15876b52f88b7b109e6f1f9c58cee489
nikhil <nkmittal4994@gmail.com>
2016-04-26 18:10:37 +0530

    for ploting graphs...results taken as best values so far, iteration no.

--- Bfo/Sphere/bfo.py
+++ Bfo/Sphere/bfo.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     print("Dimension: ", dimension)
     # For tracking global minima. ;)
     results = {'c_space': c_space, 'c_prob': c_prob, 'c_tumble': c_tumble}
+    res = {}
     for i in range(num):
         best = INF                 # the best solution found during the search
         fe_count = 0               # number of objective function evaluations
@@ -19,13 +20,17 @@ if __name__ == '__main__':
         population, c_space, fe_count, best = initialize_population(
             i+1, population, c_space, fe_count, best)
         # minimization of objective function
-        best = optimization(
+        best, iteration_values = optimization(
             i+1, population, c_space, fe_count, best, c_prob, c_tumble)
         if best == 0.0:
             results[str(i+1)] = 1.0
         else:
             results[str(i+1)] = best
+            res[str(i+1)] = iteration_values
 
-    with open('data.py', 'a') as f:
-        f.write(dumps(results, sort_keys=True) + '\n')
+    # with open('data.py', 'a') as f:
+    #     f.write(dumps(results, sort_keys=True) + '\n')
+
+    with open('data_graphs.py', 'a') as f:
+        f.write(dumps(res, sort_keys=True) + '\n')
     print("Updated!")
